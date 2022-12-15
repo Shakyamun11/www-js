5ba9364036f6ff19ac37b7301a8b930c24e39729
Shakyamun11 <119165751+Shakyamun11@users.noreply.github.com>
2022-12-15 21:01:46 +0400

    mushome

---
+++ script.js
@@ -0,0 +1,6 @@
+var burgermenu = document.getElementById( ' burgermenu ')
+var nav = document.getElementById ('nav')
+
+burgermenu.addEventListener ('click', function (){
+    nav.classList.burgermenu('active')
+}) 
